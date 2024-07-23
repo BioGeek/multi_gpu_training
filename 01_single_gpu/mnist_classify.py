a4c024262d619b9a6b745e5973eee7909079c280
Jeroen Van Goey <j.vangoey@instadeep.com>
2024-07-24 00:49:20 +0200

    add line profiler decorator to train function

--- 01_single_gpu/mnist_classify.py
+++ 01_single_gpu/mnist_classify.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
+from line_profiler import profile
 
 class Net(nn.Module):
     def __init__(self):
@@ -36,7 +36,7 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-
+@profile
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
